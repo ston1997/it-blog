a55660979bb4df21e068807f4c6283c3d353aea1
ston1997 <ston1997@meta.ua>
2022-09-02 22:03:46 +0300

    branch1 commit

--- news/models.py
+++ news/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
 class News(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(max_length=150, verbose_name='название')
     content = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    photo = models.ImageField(upload_to='photos/%Y%m%d')
-    is_published = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='создано')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='обновлено')
+    photo = models.ImageField(upload_to='photos/%Y%m%d', blank=True)
+    is_published = models.BooleanField(default=True, verbose_name='опубликовано')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='категория')
 
     def __str__(self):
         return self.title
@@ -14,3 +15,17 @@ class News(models.Model):
     class Meta:
         verbose_name = 'новость'
         verbose_name_plural = 'новости'
+        ordering = ['-created_at', '-title']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, db_index=True, verbose_name='категория')
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+        ordering = ['title']
